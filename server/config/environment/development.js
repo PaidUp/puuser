67d200dc250076d9ae2dc0e5dbadf2ca0e1613ee
Ricardo Lara <riclara@gmail.com>
2018-02-12 21:06:36 -0300

    fix ci config

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -1,10 +1,12 @@
-// Development specific configuration
-// ==================================
 const mongoHost = process.env.TDUSER_MONGO_HOST || 'pu-dev-shard-00-00-4nodg.mongodb.net:27017,pu-dev-shard-00-01-4nodg.mongodb.net:27017,pu-dev-shard-00-02-4nodg.mongodb.net:27017'
-module.exports = {
+
+// All configurations will extend these options
+// ============================================
+let all = {
+  env: process.env.NODE_ENV,
   port: process.env.PORT || 9001,
   mongo: {
-    uri: 'mongodb://' + mongoHost + '/develop',
+    uri: 'mongodb://' + mongoHost + '/localhost',
     prefix: 'pu_user_',
     options: {
       user: 'pudevelop',
@@ -15,11 +17,25 @@ module.exports = {
       autoIndex: false, // Don't build indexes
       reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
       reconnectInterval: 1000, // Reconnect every 500ms
-      poolSize: 5 // Maintain up to 5 socket connections
+      poolSize: 5 // Maintain up to 10 socket connections
     }
   },
   redis: {
     host: 'develop-001.rd7ge9.0001.use1.cache.amazonaws.com',
     port: 6379
-  }
+  },
+  // Secret for session, you will want to change this and make it an environment variable
+  secrets: {
+    session: 'puproduct-secret'
+  },
+  logger: {
+    projectId: 'gothic-talent-192920',
+    logName: 'pu-product-dev-log',
+    metadata: {resource: {type: 'global'}}
+  },
+  encryptKey: 'PZ3oXv2v6Pq5HAPFI9NFbQ=='
 }
+
+// Export the config object based on the NODE_ENV
+// ==============================================
+export default all
--- server/config/environment/test.js
+++ server/config/environment/test.js
@@ -1,12 +1,10 @@
-'use strict'
+const mongoHost = process.env.TDUSER_MONGO_HOST || 'pu-dev-shard-00-00-4nodg.mongodb.net:27017,pu-dev-shard-00-01-4nodg.mongodb.net:27017,pu-dev-shard-00-02-4nodg.mongodb.net:27017'
 
-// Test specific configuration
-// ===========================
-const mongoHost =
-  process.env.TDUSER_MONGO_HOST || 'pu-dev-shard-00-00-4nodg.mongodb.net:27017,pu-dev-shard-00-01-4nodg.mongodb.net:27017,pu-dev-shard-00-02-4nodg.mongodb.net:27017'
-
-module.exports = {
-  // MongoDB connection options
+// All configurations will extend these options
+// ============================================
+let all = {
+  env: process.env.NODE_ENV,
+  port: process.env.PORT || 9001,
   mongo: {
     uri: 'mongodb://' + mongoHost + '/testing',
     prefix: 'pu_user_',
@@ -17,17 +15,25 @@ module.exports = {
       replicaSet: 'pu-dev-shard-0',
       authSource: 'admin',
       autoIndex: false, // Don't build indexes
-      reconnectTries: 2, // Never stop trying to reconnect
+      reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
       reconnectInterval: 1000, // Reconnect every 500ms
-      poolSize: 1 // Maintain up to 10 socket connections
+      poolSize: 5 // Maintain up to 10 socket connections
     }
   },
-  nodePass: {
-    me: {
-      token: 'tdschedule-secret'
-    }
+  redis: {
+    host: 'redis-13835.c16.us-east-1-3.ec2.cloud.redislabs.com',
+    port: 13835
+  },
+  // Secret for session, you will want to change this and make it an environment variable
+  secrets: {
+    session: 'puproduct-secret'
   },
   logger: {
-    logName: 'pu-product-test-log'
-  }
+    projectId: 'gothic-talent-192920',
+    logName: 'pu-product-dev-log',
+    metadata: {resource: {type: 'global'}}
+  },
+  encryptKey: 'PZ3oXv2v6Pq5HAPFI9NFbQ=='
 }
+
+export default all
